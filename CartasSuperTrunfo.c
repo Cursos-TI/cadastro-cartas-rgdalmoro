f18770e2d290f8335c507c8f2f297b0ec3c84e15
rgdalmoro <49621084+rgdalmoro@users.noreply.github.com>
2025-10-27 22:47:30 +0000

    nivel novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,13 +8,30 @@ int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
       int populacao, numero_de_pontos_turisticos;
       float area, PIB;
-      char codigo[3],nome_da_cidade[50];
+      char codigo[4], UF[3], nome_da_cidade[51];
   // Área para entrada de dados
-        printf("Digite o Código da Carta:\n");
+        printf("Digite o Código da Carta:");
         scanf ( "%s", codigo);
-
+        printf("\nDigite a UF:");
+        scanf ( "%s", UF);
+        printf("\nDigite o nome da cidade:");
+        scanf ( "%s",nome_da_cidade);
+        printf("\nDigite o número de habitantes:");
+        scanf ( "%i",&populacao);
+        printf("\nDigite a área em km2:");
+        scanf ( "%f",&area);
+        printf("\nDigite o PIB:");
+        scanf ( "%f",&PIB);        
+        printf("\nDigite o número de pontos turísticos:");
+        scanf ( "%d",&numero_de_pontos_turisticos);
   // Área para exibição dos dados da cidade
-        printf("Código da Carta: %s\n", codigo);
+        printf("\nCódigo da Carta: %s\n", codigo);
+        printf("UF: %s\n", UF);
+        printf("Cidade: %s\n", nome_da_cidade);
+        printf("Número de Habitantes: %i\n", populacao);
+        printf("Area em km2: %f\n", area);
+        printf("PIB: %f\n", PIB);
+        printf("Pontos Turísticos: %d\n", numero_de_pontos_turisticos);
 
 return 0;
 
